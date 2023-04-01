76d4de1afe5f723810d808b1b87ac50b214425e4
Thomas Liu <tyangliu@gmail.com>
2023-03-31 22:46:03 -0700

    Create dialogue triangles

--- src/splash/App.tsx
+++ src/splash/App.tsx
@@ -1051,7 +1051,45 @@ export default function App() {
               border: 1px solid rgba(0,0,0,0.1);
               box-shadow: 0 1px 6px rgba(0,0,0,0.05);
               max-width: 360px;
+              position: relative;
+              background: #fff;
             `}>
+              <div css={css`
+                filter: blur(3px);
+                position: absolute;
+                width: 0;
+                height: 0;
+                border-left: 14px solid transparent;
+                border-right: 14px solid rgba(0,0,0,0.05);
+                border-bottom: 14px solid transparent;
+                right: -9.5px;
+                top: 24px;
+                transform: rotate(45deg);
+                z-index: -1;
+              `}/>
+              <div css={css`
+                position: absolute;
+                width: 0;
+                height: 0;
+                border-left: 14px solid transparent;
+                border-right: 14px solid rgba(234,234,234,1);
+                border-bottom: 14px solid transparent;
+                right: -9.5px;
+                top: 24px;
+                transform: rotate(45deg);
+                z-index: -1;
+              `}/>
+              <div css={css`
+                position: absolute;
+                width: 0;
+                height: 0;
+                border-left: 14px solid transparent;
+                border-right: 14px solid #fff;
+                border-bottom: 14px solid transparent;
+                right: -8.5px;
+                top: 24px;
+                transform: rotate(45deg);
+              `}/>
               <h2 css={css`
                 margin-bottom: 1.2rem;
               `}>
@@ -1095,14 +1133,52 @@ export default function App() {
               border: 1px solid rgba(0,0,0,0.1);
               box-shadow: 0 1px 6px rgba(0,0,0,0.05);
               max-width: 360px;
+              position: relative;
+              background: #fff;
             `}>
+              <div css={css`
+                filter: blur(3px);
+                position: absolute;
+                width: 0;
+                height: 0;
+                border-right: 14px solid transparent;
+                border-left: 14px solid rgba(0,0,0,0.05);
+                border-bottom: 14px solid transparent;
+                left: -9.5px;
+                top: 24px;
+                transform: rotate(-45deg);
+                z-index: -1;
+              `}/>
+              <div css={css`
+                position: absolute;
+                width: 0;
+                height: 0;
+                border-right: 14px solid transparent;
+                border-left: 14px solid rgba(234,234,234,1);
+                border-bottom: 14px solid transparent;
+                left: -9.5px;
+                top: 24px;
+                transform: rotate(-45deg);
+                z-index: -1;
+              `}/>
+              <div css={css`
+                position: absolute;
+                width: 0;
+                height: 0;
+                border-right: 14px solid transparent;
+                border-left: 14px solid #fff;
+                border-bottom: 14px solid transparent;
+                left: -8.5px;
+                top: 24px;
+                transform: rotate(-45deg);
+              `}/>
               <h2 css={css`
                 margin-bottom: 1.2rem;
               `}>
                 Semantically search & converse with AI
               </h2>
               <p>
-                Quickly find and save answers from webpages
+                Quickly discover and save answers from webpages
                 with the help of semantic in-page search
                 and conversational AI.
               </p>
